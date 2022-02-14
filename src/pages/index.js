3e8923be2141bc6856e061f08a70a760a6f4f172
puci2020 <adam.pucicki97@gmail.com>
2022-02-14 11:54:01 +0100

    Fix lazy load

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import {Suspense} from 'react';
 import {Helmet} from 'react-helmet';
 import styled from "styled-components";
 import Loader from "../components/Loader/Loader";
-import { LightgalleryProvider } from "react-lightgallery";
+import {LightgalleryProvider} from "react-lightgallery";
 import "lightgallery.js/dist/css/lightgallery.css";
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
@@ -26,35 +26,40 @@ const Main = styled.div`
 
 // markup
 const IndexPage = () => {
+  const isSSR = typeof window !== "undefined";
   const store = createStore(allReducers);
 
   return (
-    <Suspense fallback={<Loader />}>
-    <LightgalleryProvider>
-      <Provider store={store}>
+    <>
+      {isSSR ? (
+        <Suspense fallback={<Loader/>}>
+          <LightgalleryProvider>
+            <Provider store={store}>
 
-        <Helmet>
-          <meta charSet="utf-8"/>
-          <title>Pixel Perfect</title>
-        </Helmet>
-        <Main>
-          {/*<Hero>*/}
-          <Sidebar/>
-          <Home/>
+              <Helmet>
+                <meta charSet="utf-8"/>
+                <title>Pixel Perfect</title>
+              </Helmet>
+              <Main>
+                {/*<Hero>*/}
+                <Sidebar/>
+                <Home/>
 
-          <Header/>
-          {/*</Hero> */}
-          <Services/>
-          <Features/>
-          <Team/>
-          <News/>
-          <Gallery/>
-          <Footer/>
-        </Main>
+                <Header/>
+                {/*</Hero> */}
+                <Services/>
+                <Features/>
+                <Team/>
+                <News/>
+                <Gallery/>
+                <Footer/>
+              </Main>
 
-      </Provider>
-    </LightgalleryProvider>
-    </Suspense>
+            </Provider>
+          </LightgalleryProvider>
+        </Suspense>
+      ) : ''}
+    </>
   )
 }
 
